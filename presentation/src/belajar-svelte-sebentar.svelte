1377f761163a3391a53fca7d28750f6114002e3b
ahmadrosid <alahmadrosid@gmail.com>
2023-11-25 03:58:15 +0700

    feat: add draft presentation belajar-svelte-sebentar

---
+++ presentation/src/belajar-svelte-sebentar.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+	import { Presentation, Slide, Code, Markdown } from '@components'
+	import { Title } from '@ui'
+</script>
+
+<Presentation>
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Belajar Svelte Sebentar</span>
+		</Title>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-6xl">Apa Itu Svelte?</span>
+		</Title>
+		<div class="py-12">
+			<img src="https://i.giphy.com/media/ghuvaCOI6GOoTX0RmH/giphy.webp" alt="WHAT" class="mx-auto rounded-md" width="400" />
+		</div>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-6xl text-gray-400">Apa Itu Svelte?</span>
+		</Title>
+		<p>Frontend Framework (react, vue, angular)</p>
+	</Slide>
+	
+	<Slide animate>
+		<Title>
+			<span class="text-6xl text-gray-500">Apa Itu Svelte?</span>
+		</Title>
+		<p class="text-gray-400">Frontend Framework (react, vue, angular)</p>
+		<p class="py-8 text-7xl">Dibuat oleh Rich Harris</p>
+	</Slide>
+	
+	<Slide animate>
+		<Title>
+			<span class="text-6xl text-gray-500">Apa Itu Svelte?</span>
+		</Title>
+		<p class="text-gray-400">Frontend Framework (react, vue, angular)</p>
+		<p class="text-gray-400 pt-8 text-5xl">Dibuat oleh Rich Harris</p>
+		<p class="py-8 text-7xl">🤯 No virtual dom.</p>
+	</Slide>
+	
+	<Slide animate>
+		<div class="py-12 flex justify-center">
+            <img src="https://i.giphy.com/media/l2JhtKtDWYNKdRpoA/giphy.webp" alt="why" class="rounded-md outline outline-4 outline-gray-400" />
+        </div>
+	</Slide>
+	
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Kenapa?</span>
+		</Title>
+	</Slide>
+	
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Kenapa?</span>
+		</Title>
+		<p class="pt-8 text-3xl">Bukan sekedar framework, tetapi bahasa dan compiler.</p>
+	</Slide>
+	
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Kenapa?</span>
+		</Title>
+		<p class="pt-8 text-3xl text-gray-500">Bukan sekedar framework, tetapi bahasa dan compiler.</p>
+		<p class="pt-8 text-2xl text-gray-300">Menulis kode Svelte mirip dengan menulis HTML, CSS, dan JavaScript biasa.</p>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Keunggulan Svelte</span>
+		</Title>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Keunggulan Svelte</span>
+		</Title>
+        <p>Better Developer Experience</p>
+	</Slide>
+
+    <Slide animate>
+		<Title>
+			<span class="text-7xl">Keunggulan Svelte</span>
+		</Title>
+        <p class="text-gray-500">Better Developer Experience</p>
+        <p class="pt-8">Smaller code to ship</p>
+	</Slide>
+
+    <Slide animate>
+		<Title>
+			<span class="text-7xl">Keunggulan Svelte</span>
+		</Title>
+        <p class="text-gray-500">Better Developer Experience</p>
+        <p class="pt-8 text-gray-500">Smaller code to ship</p>
+        <p class="pt-8">Just JavaScript with Native DOM</p>
+	</Slide>
+
+	<Slide animate>
+		<Title>🎉 Developer Happy</Title>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">In Fact</span>
+        </Title>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-7xl text-gray-500">In Fact</span>
+        </Title>
+        <p class="pt-4">Presentasi Ini Dibuat dengan Svelte</p>
+
+		<div class="py-12 flex justify-center">
+            <img src="https://i.giphy.com/media/JqDeI2yjpSRgdh35oe/giphy.webp" alt="tada" />
+        </div>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-7xl">Mari kita coba</span>
+        </Title>
+	</Slide>
+
+	<Slide animate>
+		<Title>
+			<span class="text-6xl text-gray-100">Simple task manager</span>
+		</Title>
+		<div class="py-12 flex justify-center">
+            <img src="http://res.cloudinary.com/dr15yjl8w/image/upload/v1700856848/public/e3jp6asodvezayq9q323.png" alt="why" class="rounded-md outline outline-4 outline-gray-400" />
+        </div>
+	</Slide>
+
+</Presentation>
--- presentation/src/main.ts
+++ presentation/src/main.ts
@@ -1,5 +1,5 @@
 // import Slides from './laravel-beginner-mistake.svelte'
-import Slides from './kalau-kamu-bingung-mau-kerja-apa.svelte'
+import Slides from './belajar-svelte-sebentar.svelte'
 import '@styles/tailwind.css'
 
 const app = new Slides({
